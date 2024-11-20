f282deb1693349116ee4f890e5645542c2134dea
Shannon Ashford <email.sa.coder@gmail.com>
2024-11-20 01:37:04 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string hisName = "Jeff";
+            int hisAge = 34;
+            char hisChar = 'J';
+            bool isMale = true;
+            double hisDouble = -76543.21d;
+            decimal hisDecimal = 12345.67m;
+
+            Console.WriteLine($"My Name is {hisName}. I am {hisAge}, and I prefer to be called {hisChar}. I am {isMale}. My favorite numbers are: {hisDouble} and {hisDecimal}");
+
+
+
+
+
         }
     }
 }
